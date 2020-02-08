d38bf36a2972745c370e2d4dc0a047fb85d4d714
Testing_author <testing_beta@mail.com>
2020-02-08 14:04:39 +0200

    minor twitches

--- Word scramble app/ViewController.swift
+++ Word scramble app/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n") // separated by a line break
@@ -25,8 +25,24 @@ class ViewController: UITableViewController {
         if allWords.isEmpty {
             allWords = ["No words available"]
         }
+        
+        startGame()
     }
-
-
+    
+    func startGame() {
+        title = allWords.randomElement() //the view control title
+        usedWords.removeAll(keepingCapacity: true)  //removes all the values that the user has already guess so far
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
